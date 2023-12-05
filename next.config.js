d3ad31a6981aed198528b9308f28ed8c6a5408a6
Brickwall <louie.waterfall@hotmail.co.uk>
2023-12-05 19:59:33 +0000

    Fixed exporting and images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "media.gmodstore.com" },
       { protocol: "https", hostname: "i.imgur.com" }
