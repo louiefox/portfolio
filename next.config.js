6f4256a1f2c052ce97cde2510712a446c8da0ad9
Brickwall <louie.waterfall@hotmail.co.uk>
2026-04-01 22:32:32 +0100

    Removed gmodstore/imgur protocols

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: "https", hostname: "media.gmodstore.com" },
-      { protocol: "https", hostname: "i.imgur.com" }
-    ]
-  },
+  output: 'export'
 };
 
 module.exports = nextConfig;
